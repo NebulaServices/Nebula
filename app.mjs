670544eb07d7e7572fc9aeb648d3d7c5aa7d3954
Cohen Erickson <cohenerickson@gmail.com>
2022-07-25 14:11:42 -0500

    Fix Deployment

--- app.mjs
+++ app.mjs
@@ -1,11 +1,18 @@
-import Server from 'bare-server-node';
+import createBareServer from '@tomphttp/bare-server-node';
 import http from 'http';
 import https from 'https';
 import nodeStatic from 'node-static';
 import fs from 'fs';
 import * as custombare from './static/customBare.mjs';
 
-const bare = new Server('/bare/', '');
+const bareServer = createBareServer('/bare/', {
+	logErrors: false,
+	localAddress: undefined,
+	maintainer: {
+		email: 'tomphttp@sys32.dev',
+		website: 'https://github.com/tomphttp/',
+	},
+});
 
 const serve = new nodeStatic.Server('static/');
 const patronServe = new nodeStatic.Server('static/');
@@ -35,16 +42,22 @@ httpsServer.on('upgrade', upgrade);
 function request (request, response) {
     if (custombare.route(request, response)) return true;
     
-    if (bare.route_request(request, response)) return true;
-    serve.serve(request, response);
+    if (bareServer.shouldRoute(request)) {
+	    bareServer.routeRequest(request, response);
+	} else {
+	    serve.serve(request, response);
+	}
 }
 
 function upgrade (req, socket, head) {
-    if (bare.route_upgrade(req, socket, head))
-        return;
-
-    socket.end();
+    if (bareServer.shouldRoute(req)) {
+		bareServer.routeUpgrade(req, socket, head);
+	} else {
+		socket.end();
+	}
 }
 
 httpServer.listen(80);
 httpsServer.listen(443);
+
+console.log("Server running on port 80 and 443.");
