512ecbf44bfa8a9ca40fc6b7a8dfff59d462a6b3
Cohen Erickson <cohenerickson@gmail.com>
2023-02-05 18:29:22 +0000

    Update nodejs version requirements

--- replit.nix
+++ replit.nix
@@ -1,8 +1,8 @@
 { pkgs }: {
-	deps = [
-		pkgs.nodejs-16_x
-        pkgs.nodePackages.typescript-language-server
-        pkgs.yarn
-        pkgs.replitPackages.jest
-	];
+  deps = [
+    pkgs.nodejs-18_x
+    pkgs.nodePackages.typescript-language-server
+    pkgs.yarn
+    pkgs.replitPackages.jest
+  ];
 }
