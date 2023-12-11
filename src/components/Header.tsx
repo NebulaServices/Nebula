bb68711a482273fc2cf62bbca1c0838a666e2744
rift <117926989+Riftriot@users.noreply.github.com>
2023-12-10 19:44:18 -0600

    this timer is stupud im not doing it

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,21 +2,17 @@ import { useLocation } from "preact-iso";
 import LinkSvg from "../assets/link.svg";
 import LogoSvg from "../assets/logo.svg";
 import { useState, useEffect } from "preact/hooks"
-import Clock from "./Clock";
+
 export function Header() {
   const { url } = useLocation();
 
   return (
     <div id="navbar" className="h-16 px-4 bg-navbar-color flex flex-row items-center justify-between">
-			<div className="flex flex-row items-center w-1/3">
+			<div className="flex flex-row items-center w-1/2">
 				<img src="/logo.png" className="h-16 w-16"></img>
 				<h1 className="font-roboto text-text-color text-4xl">nebula.</h1>
 			</div>
-      
-			<div className="w-1/3">
-				<Clock />
-			</div>
-			<div className="w-1/3">
+			<div className="w-1/2">
 				<span className="">Games</span>
 			</div>
     </div>
