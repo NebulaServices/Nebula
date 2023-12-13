0e113e57e32aba32a06b9c082c91fb120561777d
illusionTBA <illusionztba@gmail.com>
2023-12-13 15:25:47 -0500

    import EN -> en

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import translationEN from './locales/EN.json';
+import translationEN from './locales/en.json';
 import translationJA from './locales/ja.json';
 
 const resources = {
