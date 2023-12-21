765f27f4f3faeb7f06614bb7014fcaae29d28b44
rift <117926989+Riftriot@users.noreply.github.com>
2023-12-20 22:20:13 -0600

    footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,7 +18,15 @@ export function Home() {
       <Helmet>
         <title>Nebula</title>
       </Helmet>
-      <div class="flex h-full items-center justify-center">
+      <div class="flex h-full flex-col items-center justify-center">
+        <div class="font-inter absolute bottom-0 left-0 p-1 text-sm italic text-input-text">
+          Nebula &copy; Nebula Services {new Date().getUTCFullYear()}
+        </div>
+        <a href="https://github.com/NebulaServices/Nebula">
+          <div class="font-inter absolute bottom-0 right-0 p-1 text-sm text-input-text">
+            GitHub
+          </div>
+        </a>
         <form
           onSubmit={handleSubmit}
           class="flex h-full w-full items-center justify-center"
@@ -32,7 +40,9 @@ export function Home() {
             }}
             type="text"
             value={inputValue}
-            onChange={(e) => setInputValue((e.target as HTMLInputElement).value)}
+            onChange={(e) =>
+              setInputValue((e.target as HTMLInputElement).value)
+            }
             className={`font-roboto h-14 rounded-2xl border border-input-border-color bg-input p-2 text-center text-xl placeholder:text-input-text focus:outline-none ${
               isFocused ? "w-full md:w-3/12" : "w-full md:w-80"
             } transition-all duration-300`}
