5f27a4bc920fe49f7fedcafb26b16419dfae4627
rift <117926989+Riftriot@users.noreply.github.com>
2023-12-22 16:07:45 -0600

    add open in settings (no functionality)

--- src/pages/Settings/Proxy.tsx
+++ src/pages/Settings/Proxy.tsx
@@ -5,11 +5,18 @@ import { useTranslation } from "react-i18next";
 
 const Proxy = ({ id, active }) => {
   const { t } = useTranslation();
+
   const engines = [
     { id: "automatic", label: t("settings.proxy.automatic") },
     { id: "ultraviolet", label: "Ultraviolet" },
     { id: "rammerhead", label: "Rammerhead" },
     { id: "dynamic", label: "Dynamic " + t("settings.proxy.buggyWarning") }
+  ];
+
+  const proxyModes = [
+    { id: "embed", label: t("settings.proxymodes.embed") },
+    { id: "direct", label: t("settings.proxymodes.direct") },
+    { id: "aboutblank", label: t("settings.proxymodes.aboutblank") }
   ];
 
   return (
@@ -25,10 +32,15 @@ const Proxy = ({ id, active }) => {
         variants={settingsPageVariant}
         className="content-card flex flex-row flex-wrap justify-around"
       >
-        <div class="bg-lighter flex h-64 w-80 flex-col flex-wrap content-center items-center rounded-lg border border-input-border-color p-7 text-center">
+        <div class="flex h-64 w-80 flex-col flex-wrap content-center items-center rounded-lg border border-input-border-color bg-lighter p-7 text-center">
           <div class="p-2 text-3xl">{t("settings.proxy.title")}</div>
           <div class="text-md p-4">{t("settings.proxy.subtitle")}</div>
           <Dropdown storageKey="proxy" options={engines} refresh={false} />
+        </div>
+        <div class="flex h-64 w-80 flex-col flex-wrap content-center items-center rounded-lg border border-input-border-color bg-lighter p-7 text-center">
+          <div class="p-2 text-3xl">{t("settings.proxymodes.title")}</div>
+          <div class="text-md p-4">{t("settings.proxymodes.subtitle")}</div>
+          <Dropdown storageKey="proxyMode" options={proxyModes} refresh={false} />
         </div>
       </motion.div>
     </motion.div>
