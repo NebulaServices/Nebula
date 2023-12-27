1c48eb2a658d7fa8beb4d6d85a2e314cfd1afc1a
rift <117926989+Riftriot@users.noreply.github.com>
2023-12-27 14:19:48 -0600

    basic suspense

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,20 @@
 import { render } from "preact";
-import { LocationProvider, Router, Route } from "preact-iso";
-import { Home } from "./pages/Home";
-import { NotFound } from "./pages/_404.jsx";
-import { DiscordPage } from "./pages/discord.jsx";
-import { ProxyFrame } from "./pages/ProxyFrame.js";
-import { Settings } from "./pages/Settings/index.js";
+import { Suspense, lazy } from "preact/compat";
+const Routes = lazy(() => import("./routes"));
 
-import "./style.css";
-import "./themes/main.css";
-import "./i18n";
-
-export function App() {
+export default function App() {
   return (
-    <LocationProvider>
-      <Router>
-        <Route path="/" component={Home} />
-        <Route path="/discord" component={DiscordPage} />
-        <Route path="/go/:url" component={ProxyFrame} />
-        <Route path="/settings" component={Settings} />
-        <Route default component={NotFound} />
-      </Router>
-    </LocationProvider>
+    <Suspense
+      fallback={
+        <div>
+          <div>loading...</div>
+        </div>
+      }
+    >
+      <div>
+        <Routes />
+      </div>
+    </Suspense>
   );
 }
 
