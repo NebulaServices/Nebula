c954fab09721ac473a39b5024b64e727f0d225dc
rift <117926989+Riftriot@users.noreply.github.com>
2023-12-27 15:09:49 -0600

    add suspense

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,11 @@
 import { render } from "preact";
 import { Suspense, lazy } from "preact/compat";
+import { LoadSuspense } from "./LoadSuspense";
 const Routes = lazy(() => import("./routes"));
 
 export default function App() {
   return (
-    <Suspense
-      fallback={
-        <div>
-          <div>loading...</div>
-        </div>
-      }
-    >
+    <Suspense fallback={<LoadSuspense />}>
       <div>
         <Routes />
       </div>
