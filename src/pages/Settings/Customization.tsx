6db77d4785eef00dc171566fd614ef6d173b0fac
rift <117926989+Riftriot@users.noreply.github.com>
2023-12-27 14:40:51 -0600

    Coming soon locales

--- src/pages/Settings/Customization.tsx
+++ src/pages/Settings/Customization.tsx
@@ -1,20 +1,27 @@
 import { motion } from "framer-motion";
 import { tabContentVariant, settingsPageVariant } from "./Variants";
+import { useTranslation } from "react-i18next";
 
-const Customization = ({ id, active }) => (
-  <motion.div
-    role="tabpanel"
-    id={id}
-    className="tab-content"
-    variants={tabContentVariant}
-    animate={active ? "active" : "inactive"}
-    initial="inactive"
-  >
-    <motion.div variants={settingsPageVariant} className="content-card text-center w-full flex flex-col justify-center items-center">
-      <img src="/comingsoonsnake.png" class="h-72 w-72"></img>
-      <h1 class="font-roboto text-3xl">Coming soon!</h1>
+function Customization({ id, active }) {
+  const { t } = useTranslation();
+  return (
+    <motion.div
+      role="tabpanel"
+      id={id}
+      className="tab-content"
+      variants={tabContentVariant}
+      animate={active ? "active" : "inactive"}
+      initial="inactive"
+    >
+      <motion.div
+        variants={settingsPageVariant}
+        className="content-card flex w-full flex-col items-center justify-center text-center"
+      >
+        <img src="/comingsoonsnake.png" class="h-72 w-72"></img>
+        <h1 class="font-roboto text-3xl">{t("comingsoon")}</h1>
+      </motion.div>
     </motion.div>
-  </motion.div>
-);
+  );
+}
 
 export default Customization;
