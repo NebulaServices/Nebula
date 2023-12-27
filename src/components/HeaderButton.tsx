38a4e86effb3ce5ea99dd32ef730706b97c09390
MotorTruck1221 <tuckerj0606@icloud.com>
2023-12-27 03:17:17 -0700

    Mobile nav update

--- src/components/HeaderButton.tsx
+++ src/components/HeaderButton.tsx
@@ -14,11 +14,10 @@ export function HeaderButton(props: HeaderButtonProps) {
   return (
     <Link
       href={href}
-      className="flex h-full w-full bg-navbar-color sm:h-auto md:h-16"
-    >
-      <div className="group flex flex-row items-center p-4">
-        <Icon className="h-6 w-6 transition duration-500 group-hover:text-text-hover-color" />
-        <span className="font-roboto pl-1 text-lg font-bold text-text-color transition duration-500 group-hover:text-text-hover-color">
+      className="flex h-full w-full bg-navbar-color sm:h-auto">
+      <div className="group flex flex-row items-center justify-center p-4 w-full border-t-2 border-solid border-navbar-text-color md:border-none">
+        <Icon className="h-10 w-10 text-text-color transition duration-500 group-hover:text-text-hover-color md:h-6 md:w-6" />
+        <span className="font-roboto pl-1 text-text-color text-4xl text-center font-bold transition duration-500 group-hover:text-text-hover-color md:text-lg">
           {t(translationKey)}
         </span>
       </div>
