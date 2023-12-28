b81e686fc270c989c0a5c6f2bbab445c8819b3b9
rift <117926989+Riftriot@users.noreply.github.com>
2023-12-27 18:55:14 -0600

    radon games and discord emebed tags

---
+++ src/pages/Radon.tsx
@@ -0,0 +1,9 @@
+import { HeaderRoute } from "../components/HeaderRoute";
+
+export function Radon() {
+  return (
+    <HeaderRoute>
+      <iframe src="/~/uv/https%3A%2F%2Fradon.games%2F" class="w-full h-full"></iframe>
+    </HeaderRoute>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { Home } from "./pages/Home";
 import { NotFound } from "./pages/_404.jsx";
 import { DiscordPage } from "./pages/discord.jsx";
 import { ProxyFrame } from "./pages/ProxyFrame.js";
+import { Radon } from "./pages/Radon";
 import { Settings } from "./pages/Settings/index.js";
 import { AboutBlank } from "./AboutBlank";
 import "./style.css";
@@ -15,6 +16,7 @@ export default function Routes() {
       <Router>
         <Route path="/" component={Home} />
         <Route path="/discord" component={DiscordPage} />
+        <Route path="/games" component={Radon} />
         <Route path="/go/:url" component={ProxyFrame} />
         <Route path="/settings" component={Settings} />
         <Route path="/ab/:url" component={AboutBlank} />
