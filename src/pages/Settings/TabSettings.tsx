dbe076e80113e38fa6e3141de50bc6d7824b5b99
incognitotgt <mashoor539@icloud.com>
2023-12-31 23:36:21 -0500

    about:blank for entire nebula

--- src/pages/Settings/TabSettings.tsx
+++ src/pages/Settings/TabSettings.tsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import { tabContentVariant, settingsPageVariant } from "./Variants";
 import CloakPreset from "./CloakPreset";
 import { useTranslation } from "react-i18next";
+import { LoadSuspense } from "../../LoadSuspense";
 
 const TabSettings = ({ id, active }) => {
   const { t } = useTranslation();
@@ -19,8 +20,12 @@ const TabSettings = ({ id, active }) => {
         variants={settingsPageVariant}
         className="content-card flex w-full flex-col items-center justify-center text-center"
       >
-        <div class="text-3xl font-bold text-input-text">{t("settings.cloaking.title")}</div>
-        <div class="text-md font-bold pb-5 text-input-text">{t("settings.cloaking.subtitle")}</div>
+        <div class="text-3xl font-bold text-input-text">
+          {t("settings.cloaking.title")}
+        </div>
+        <div class="text-md pb-5 font-bold text-input-text">
+          {t("settings.cloaking.subtitle")}
+        </div>
         <div class="flex flex-row space-x-4">
           <CloakPreset faviconUrl="none" title="none" />
           <CloakPreset
@@ -43,6 +48,36 @@ const TabSettings = ({ id, active }) => {
             faviconUrl="https://asset-cdn.schoology.com/sites/all/themes/schoology_theme/favicon.ico"
             title="Schoology"
           />
+        </div>
+        <div class="relative p-4">
+          <button
+            class="font-roboto h-14 w-56 rounded-2xl border border-input-border-color bg-input p-2 text-center text-xl font-bold text-input-text placeholder:text-input-text focus:outline-none"
+            onClick={() => {
+              let newWindow = window.open("about:blank");
+              let iframe = document.createElement("iframe");
+              iframe.src = window.location.origin;
+              iframe.style.width = "100%";
+              iframe.style.height = "100%";
+              iframe.style.border = "none";
+              iframe.style.overflow = "hidden";
+              iframe.style.margin = "0";
+              iframe.style.padding = "0";
+              iframe.style.position = "fixed";
+              iframe.style.top = "0";
+              iframe.style.bottom = "0";
+              iframe.style.left = "0";
+              iframe.style.right = "0";
+              newWindow.document.body.appendChild(iframe);
+              window.location.replace("https://google.com");
+              return (
+                <div>
+                  <LoadSuspense />
+                </div>
+              );
+            }}
+          >
+            {t("settings.cloaking.aboutblank")}
+          </button>
         </div>
       </motion.div>
     </motion.div>
