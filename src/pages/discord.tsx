d8a975d531f72c3ee00d87033224f96e03e71c70
incognitotgt <mashoor539@icloud.com>
2024-01-02 17:51:56 -0500

    Catppuccin theming was inconsistent

--- src/pages/discord.tsx
+++ src/pages/discord.tsx
@@ -15,13 +15,15 @@ export function DiscordPage() {
         <div className="flex h-full flex-col items-center justify-center">
           <img src="/services.png" className="h-72 w-72"></img>
           <div className="flex flex-col items-center p-6">
-            <p className="font-roboto text-4xl font-bold">
+            <p className="font-roboto text-4xl font-bold text-input-text">
               {t("discord.title")}
             </p>
-            <span className="font-roboto text-3xl">{t("discord.sub")}</span>
+            <span className="font-roboto text-3xl text-input-text">
+              {t("discord.sub")}
+            </span>
           </div>
           <a href="https://discord.gg/unblocker" className="p-6">
-            <button className="font-roboto h-14 w-56 rounded-2xl border border-input-border-color bg-input p-2 text-center text-xl placeholder:text-input-text focus:outline-none">
+            <button className="font-roboto h-14 w-56 rounded-2xl border border-input-border-color bg-input p-2 text-center text-xl text-input-text focus:outline-none">
               {t("discord.button1")}
             </button>
           </a>
@@ -29,7 +31,7 @@ export function DiscordPage() {
             href="/~/uv/https%3A%2F%2Fdiscord.com%2Finvite%2Funblocker"
             className="p-6"
           >
-            <button className="font-roboto h-14 w-56 rounded-2xl border border-input-border-color bg-input p-2 text-center text-xl placeholder:text-input-text focus:outline-none">
+            <button className="font-roboto h-14 w-56 rounded-2xl border border-input-border-color bg-input p-2 text-center text-xl text-input-text focus:outline-none">
               {t("discord.button2")}
             </button>
           </a>
