38b404d453f16209536e671c4d65c4c7799bee43
MotorTruck1221 <tuckerj0606@icloud.com>
2024-01-02 23:44:02 -0700

    Export get (for use later)

--- src/util/IDB.js
+++ src/util/IDB.js
@@ -8,6 +8,16 @@ function set(key, value) {
     })
     localforage.setItem(key, value);
 }
+async function get(key) {
+    localforage.config({
+        driver: localforage.INDEXEDDB,
+        name: 'Nebula',
+        version: 1.0,
+        storeName: 'nebula_config',
+        description: 'Nebula Config for things reliant on IndexedDB'
+    })
+    return await localforage.getItem(key);
+}
 
  
-export { set };
+export { set, get }
