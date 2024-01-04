6c6c6626cfa8040eb2e3a49348d6e8755aba5952
Riftriot <slushziesrazaxc@gmail.com>
2024-01-04 14:31:52 -0600

    Localize bare switcher and add button

--- src/pages/Settings/BareInput.tsx
+++ src/pages/Settings/BareInput.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "preact/hooks";
 import { set } from "../../util/IDB";
 import { uninstallServiceWorkers } from "../../util/SWHelper";
+import { useTranslation } from "react-i18next";
 
 interface BareInputProps {
   placeholder: string;
@@ -8,6 +9,7 @@ interface BareInputProps {
 }
 
 function BareInput(props: BareInputProps) {
+  const { t } = useTranslation();
   const value = localStorage.getItem(props.storageKey) || "/bare/";
   const [inputValue, setInputValue] = useState(value);
   function validateUrl(url: string) {
@@ -28,22 +30,29 @@ function BareInput(props: BareInputProps) {
     }
     return finalUrl;
   }
-  function handleChange(event: any) {
-    const url = validateUrl(event.target.value);
-    setInputValue(event.target.value);
+  function handleChange() {
+    const url = validateUrl((document.getElementById("input") as HTMLInputElement).value);
+    setInputValue((document.getElementById("input") as HTMLInputElement).value);
     set(props.storageKey, url);
     localStorage.setItem(props.storageKey, url);
     uninstallServiceWorkers();
     window.location.reload();
   }
   return (
-    <input
-      type="text"
-      placeholder={props.placeholder}
-      value={inputValue}
-      onBlur={handleChange}
-      className="font-roboto flex h-14 w-56 flex-row rounded-2xl border border-input-border-color bg-input p-4 text-center text-xl"
-    />
+    <div className="flex flex-col items-center">
+      <input
+        type="text"
+        placeholder={props.placeholder}
+        value={inputValue}
+        id="input"
+        className="font-roboto flex h-14 w-56 flex-row rounded-2xl border border-input-border-color bg-input p-4 text-center text-xl"
+      />
+      <div className="font-roboto items-center justify-center flex h-4 w-24 flex-row rounded-2xl border border-input-border-color bg-input p-4 text-center text-xl" 
+      onClick={handleChange}
+      >
+        {t("settings.bare.select")}
+      </div>
+    </div>
   );
 }
 
