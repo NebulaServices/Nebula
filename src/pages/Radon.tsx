c3067d3783ecf7cc8a2430134216cbf17b72d201
Violet <153395462+Entrpix@users.noreply.github.com>
2024-01-07 14:13:23 -0500

    Update Radon.tsx
    Made the proxy the iframe uses set to selected proxy

--- src/pages/Radon.tsx
+++ src/pages/Radon.tsx
@@ -1,14 +1,36 @@
 import { HeaderRoute } from "../components/HeaderRoute";
-interface Window {
-  __uv$config: any;
+declare global {
+  interface Window {
+    __uv$config: any;
+    __dynamic$config: any;
+  }
 }
+
+export function gameProxy() {
+  const localProxy = localStorage.getItem("proxy") || "automatic";
+
+  if (localProxy == "ultraviolet") {
+    return window.__uv$config.prefix + window.__uv$config.encodeUrl("https://radon.games")
+  };
+
+  if (localProxy == "dynamic") {
+   // return window.__dynamic$config.prefix + window.__dynamic$config.encoding("https://radon.games") 
+    return window.__dynamic$config.prefix + "route?url=" + encodeURIComponent("https://radon.games")
+  }
+
+  else {
+    return window.__uv$config.prefix + window.__uv$config.encodeUrl("https://radon.games")
+  }
+};
+
+  
 export function Radon() {
   return (
     <HeaderRoute>
+      
       <iframe
         src={
-          window.__uv$config.prefix +
-          window.__uv$config.encodeUrl("https://radon.games")
+          gameProxy()
         }
         className="h-full w-full"
       ></iframe>
