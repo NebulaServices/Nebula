c9ce4b5d1adac92cb25aa92d2dd434618d8062a6
rift <117926989+Riftriot@users.noreply.github.com>
2024-03-12 13:15:21 -0500

    Revert "Set libcurl as default on ios and update libcurl"
    This reverts commit 9357f2ed11d350f921d01e671c849985b31d803b.

--- src/util/transports.ts
+++ src/util/transports.ts
@@ -16,7 +16,7 @@ function changeTransport(transport: string, wispUrl: string) {
       console.log("Setting transport to Libcurl");
       SetTransport("CurlMod.LibcurlClient", {
         wisp: wispUrl,
-        wasm: "https://cdn.jsdelivr.net/npm/libcurl.js@v0.5.3/libcurl.wasm"
+        wasm: "https://cdn.jsdelivr.net/npm/libcurl.js@v0.5.2/libcurl.wasm"
       });
       break;
     case "bare":
@@ -46,9 +46,17 @@ const wispUrl =
   "/wisp/";
 registerRemoteListener(navigator.serviceWorker.controller!);
 
-changeTransport(
-  localStorage.getItem("transport") || "libcurl",
-  localStorage.getItem("wispUrl") || wispUrl
-);
+if (isIOS) {
+  console.log("iOS device detected. Bare will be used.");
+  changeTransport(
+    localStorage.getItem("transport") || "libcurl",
+    localStorage.getItem("wispUrl") || wispUrl
+  );
+} else {
+  changeTransport(
+    localStorage.getItem("transport") || "bare",
+    localStorage.getItem("wispUrl") || wispUrl
+  );
+}
 
 export { changeTransport, getTransport };
