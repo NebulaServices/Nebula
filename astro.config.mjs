0262ee9ff06b7685231dc73dd2e03a814741f53d
rift <117926989+Riftriot@users.noreply.github.com>
2024-07-24 15:38:59 -0500

    Fix the dev server????

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  i18n: {
+    defaultLocale: "en-US",
+    locales: ["en-US", "jp"],
+  },
 });
