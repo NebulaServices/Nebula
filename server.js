e606fc99859c6399c2b0aca0b9a785ed063f0f2b
rift <117926989+Riftriot@users.noreply.github.com>
2024-08-01 22:48:40 -0500

    add sequelize and init nebula catalog

--- server.js
+++ server.js
@@ -1,11 +1,44 @@
 import fastify from "fastify";
 import fastifyStatic from "@fastify/static";
 import path from "path";
+import { Sequelize, DataTypes } from "sequelize";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = fastify({ logger: false });
+const sequelize = new Sequelize("database", "user", "password", {
+  host: "localhost",
+  dialect: "sqlite",
+  logging: false,
+  // SQLite only
+  storage: "database.sqlite",
+});
+
+const catalog_assets = sequelize.define("catalog_assets", {
+  package_name: {
+    type: DataTypes.TEXT,
+    unique: true,
+  },
+  title: {
+    type: DataTypes.TEXT,
+  },
+  description: {
+    type: DataTypes.TEXT,
+  },
+  tags: {
+    type: DataTypes.JSON,
+  },
+  image: {
+    type: DataTypes.TEXT,
+  },
+  script: {
+    type: DataTypes.TEXT,
+  },
+  type: {
+    type: DataTypes.TEXT,
+  },
+});
 
 await app.register(import("@fastify/compress"));
 
