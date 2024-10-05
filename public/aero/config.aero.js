19a2dea153e31419d7473d4d2cfbb6cdbff0b9b5
Ryan Wilson <inbox@ryanwilson.space>
2024-10-05 15:35:10 -0400

    Update aero's config to reflect recent changes (default config bundled)

--- public/aero/config.aero.js
+++ public/aero/config.aero.js
@@ -1,56 +1,8 @@
 /** @import { Config } from "aero-proxy" */
 
 /**
- * @type {string}
- */
-const escapeKeyword = "_";
-/**
- * @type {string}
- */
-const dirToAero = "/aero/";
-
-/**
  * @type {Config}
  */
 self.aeroConfig = {
-	bc: new BareMux(),
-    serverBackendPrefix: "/bare/", 
-    prefix: "/~/aero/",
-	pathToInitialSW: "/sw.js",
-	bundles: {
-		"bare-mux": `${dirToAero}BareMux.aero.js`,
-		handle: `${dirToAero}sw.aero.js`,
-		sandbox: `${dirToAero}sandbox/sandbox.aero.js`
-	},
-	aeroPathFilter: path =>
-		Object.values(self.config.bundles).find(bundlePath =>
-			path.startsWith(bundlePath)
-		) === null ||
-		path.startsWith("/tests/") ||
-		path.startsWith("/baremux") ||
-		path.startsWith("/epoxy/") ||
-		!path.startsWith(aeroConfig.prefix),
-	searchParamOptions: {
-		referrerPolicy: {
-			escapeKeyword,
-			searchParam: "passthroughReferrerPolicy"
-		},
-		isModule: {
-			escapeKeyword,
-			searchParam: "isModule"
-		},
-		integrity: {
-			escapeKeyword,
-			searchParam: "integrity"
-		}
-	},
-	cacheKey: "httpCache",
-	dynamicConfig: {
-		dbName: "aero",
-		id: "update"
-	},
-	//urlEncoder: __uv$config.urlEncoder,
-	//urlDecoder: __uv$config.urlDecoder,
-	urlEncoder: url => url,
-	urlDecoder: url => url
+	...self.defaultAeroConfig
 };
---
+++ public/aero/sandbox/config.js
@@ -0,0 +1,13 @@
+/** @import { Config } from "aero-sandbox" */
+
+import { defaultConfig } from "./defaultConfig.js";
+
+/**
+ * @type {Config}
+ */
+const config = {
+	...defaultConfig,
+	prefix: "/~/aero/"
+};
+
+export default config;
