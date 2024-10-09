5fe93963f35ee13c52ba653db1851c91708330f7
MotorTruck1221 <motortruck1221@protonmail.com>
2024-10-09 01:50:23 -0600

    Rift, whatever autoformatter you are using is giving me cancer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,13 +3,13 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
-      primary: "var(--background-primary)",
-      lighter: "var(--background-lighter)",
+      "primary": "var(--background-primary)",
+      "lighter": "var(--background-lighter)",
       "navbar-text-color": "var(--navbar-text-color)",
       "navbar-color": "var(--navbar-color)",
       "text-color": "var(--navbar-link-color)",
       "text-hover-color": "var(--navbar-link-hover-color)",
-      input: "var(--input-background-color)",
+      "input": "var(--input-background-color)",
       "input-text": "var(--input-text-color)",
       "input-border-color": "var(--input-border-color)",
       "dropdown-option-hover-color": "var(--dropdown-option-hover-color)",
