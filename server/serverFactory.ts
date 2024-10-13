35680e719b14e9ce458a1ebd07150a245f24ba90
MotorTruck1221 <motortruck1221@protonmail.com>
2024-10-13 05:17:21 -0600

    Wisp logging bs

--- server/serverFactory.ts
+++ server/serverFactory.ts
@@ -3,6 +3,7 @@ import wisp from 'wisp-server-node';
 import rammerhead from '@rubynetwork/rammerhead';
 import { FastifyServerFactory, FastifyServerFactoryHandler, RawServerDefault } from 'fastify';
 import { parsedDoc } from './config.js';
+import { LOG_LEVEL, WispOptions } from 'wisp-server-node/dist/Types.js';
 
 const rh = rammerhead.createRammerhead({
     logLevel: parsedDoc.server.server.logging ? 'debug' : 'disabled',
@@ -10,6 +11,11 @@ const rh = rammerhead.createRammerhead({
     disableLocalStorageSync: parsedDoc.server.rammerhead.localstorage_sync ? false : true,
     disableHttp2: parsedDoc.server.rammerhead.http2 ? false : true 
 });
+
+const wispOptions: WispOptions = {
+    logLevel: parsedDoc.server.server.logging ? LOG_LEVEL.DEBUG : LOG_LEVEL.NONE,
+    pingInterval: 30
+}
 
 const serverFactory: FastifyServerFactory = (handler: FastifyServerFactoryHandler): RawServerDefault => {
     const httpServer = createServer();
@@ -27,7 +33,7 @@ const serverFactory: FastifyServerFactory = (handler: FastifyServerFactoryHandle
         }
         else if (parsedDoc.server.server.wisp) {
             if (req.url?.endsWith('/wisp/')) {
-                wisp.routeRequest(req, socket as any, head);
+                wisp.routeRequest(req, socket as any, head, wispOptions);
             }
         }
     });
