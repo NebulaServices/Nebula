de2936ad2ec26c0d2112c33b0f63a2ce7d56f2ba
Scaratek <153395462+Scaratech@users.noreply.github.com>
2025-06-17 17:20:09 -0400

    Update fix_sqlite3.sh

--- fix_sqlite3.sh
+++ fix_sqlite3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Fixes sqlite3 related issues (ARCH ONLY | Requires https://nvm.sh)
-
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 nvm install 20
 nvm use 20
 
@@ -14,6 +14,6 @@ rm -r node_modules/
 rm pnpm-lock.yaml
 "$pnpm" i
 
-sudo pacman -S python-distutils-extra
+sudo pacman -S python-distutils-extra # sudo apt install python3-distutils-extra -y (on Debian/Ubuntu)
 "$pnpm" rebuild sqlite3
 npx node-gyp rebuild --directory=node_modules/.pnpm/sqlite3@5.1.7/node_modules/sqlite3
