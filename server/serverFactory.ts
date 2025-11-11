b0a108d51f1259c7d477f105da46b298e0782e70
scaratech <entrpix@outlook.com>
2025-11-10 21:45:56 -0500

    chore: migrate from wisp-server-node -> @mercuryworkshop/wisp-js

--- server/serverFactory.ts
+++ server/serverFactory.ts
@@ -1,13 +1,7 @@
 import { createServer } from "node:http";
 import { FastifyServerFactory, FastifyServerFactoryHandler, RawServerDefault } from "fastify";
-import wisp from "wisp-server-node";
-import { LOG_LEVEL, WispOptions } from "wisp-server-node/dist/Types.js";
+import { server as wisp } from "@mercuryworkshop/wisp-js/server";;
 import { parsedDoc } from "./config.js";
-
-const wispOptions: WispOptions = {
-    logLevel: parsedDoc.server.server.logging ? LOG_LEVEL.DEBUG : LOG_LEVEL.NONE,
-    pingInterval: 30
-};
 
 const serverFactory: FastifyServerFactory = (
     handler: FastifyServerFactoryHandler
@@ -19,7 +13,7 @@ const serverFactory: FastifyServerFactory = (
     httpServer.on("upgrade", (req, socket, head) => {
         if (parsedDoc.server.server.wisp) {
             if (req.url?.endsWith("/wisp/")) {
-                wisp.routeRequest(req, socket as any, head, wispOptions);
+                wisp.routeRequest(req, socket as any, head);
             }
         }
     });
